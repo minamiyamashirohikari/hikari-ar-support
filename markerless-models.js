a8e1aac5d31c460295762716dc3993a56c2783ab
CodexSandboxOffline <CodexSandboxOffline@2412-0017-008.minamiyamashiro.local>
2026-07-15 10:01:17 +0900

    Make food textures compatible with iPhone AR

--- markerless-models.js
+++ markerless-models.js
@@ -6,8 +6,9 @@
 
   for (const id of window.REAL_MODEL_TARGETS || []) {
     const folder = highQuality.has(id) ? 'models_high_quality' : 'models';
+    const revision = highQuality.has(id) ? '?v=20260715-ios-png1' : '';
     window.registerRealFoodModel(id, {
-      url: `assets/${folder}/${id}.glb`,
+      url: `assets/${folder}/${id}.glb${revision}`,
       scale: '.13 .13 .13',
       rotation: '0 0 0',
       position: '0 0 0'
