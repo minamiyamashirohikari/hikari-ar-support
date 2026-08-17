057240d146d0ecd17dcf31dfd26f7182ba0ceda5
CodexSandboxOffline <CodexSandboxOffline@2412-0017-008.minamiyamashiro.local>
2026-08-17 11:12:13 +0900

    Publish chukadon texture quality update

--- markerless-models.js
+++ markerless-models.js
@@ -5,7 +5,7 @@
     'sushi', 'grilled_fish', 'tempura', 'ebi_fry', 'hamburger', 'sandwich',
     'sandwich2', 'onigiri', 'chukadon'
   ]);
-  const revision = '?v=20260817-chukadonfix17';
+  const revision = '?v=20260817-chukadontexture18';
 
   for (const id of window.REAL_MODEL_TARGETS || []) {
     const folder = priorityModels.has(id) ? 'models_high_quality' : 'models';
