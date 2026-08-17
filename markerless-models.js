334a07d0a138797bc302406d32f894031660fba7
CodexSandboxOffline <CodexSandboxOffline@2412-0017-008.minamiyamashiro.local>
2026-08-17 15:01:34 +0900

    Publish bowl-fitted gyudon model

--- markerless-models.js
+++ markerless-models.js
@@ -5,7 +5,7 @@
     'sushi', 'grilled_fish', 'tempura', 'ebi_fry', 'hamburger', 'sandwich',
     'sandwich2', 'onigiri', 'chukadon'
   ]);
-  const revision = '?v=20260817-gyudonrim38';
+  const revision = '?v=20260817-gyudonfit39';
 
   for (const id of window.REAL_MODEL_TARGETS || []) {
     const folder = priorityModels.has(id) ? 'models_high_quality' : 'models';
